02a3d2955cdd7f8ef151680b6b3269b7c884120a
V Daita <vdaita@gmail.com>
2025-01-31 22:08:15 -0600

    created math judge

--- math_judge.py
+++ math_judge.py
@@ -1,1 +1,24 @@
 from trl.trl.trainer.judges import BaseRankJudge
+from math_verify import parse, verify
+from rapidfuzz.process import extractOne
+
+class MathJudge(BaseRankJudge):
+    def __init__(self, qa_dict: dict[str, str]):
+        super(MathJudge, self).__init__()
+        self.qa_dict = qa_dict
+        self.questions = list(qa_dict.keys())
+
+    def judge(self, prompts, completions):
+        results = [0]
+        for (prompt, (ans_a, ans_b)) in zip(prompts, completions):
+            ground_truth = parse(self.qa_dict[extractOne(prompt, self.questions)[0]])
+            ans_a_correct = verify(ground_truth, parse(ans_a))
+            ans_b_correct = verify(ground_truth, parse(ans_b))
+            
+            if ans_a_correct and not ans_b_correct:
+                results.append(1)
+            elif ans_a_correct and ans_b_correct:
+                results.append(0)
+            else:
+                results.append(-1)
+        return results        
